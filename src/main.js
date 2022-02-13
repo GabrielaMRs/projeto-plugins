713a54c6cbad0f17e51fb993158c603d3825ea0d
Gabriela <gabriela.rodrigues@chatguru.com.br>
2022-02-13 09:50:09 -0300

    yes

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 createApp(App).use(store).use(router).mount('#app')
+
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+}).$mount("#app");
